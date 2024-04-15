f50d7160ad8d09f463592da332c72fb3fa988169
HusseinYasser <husseinyasser388@gmail.com>
2024-04-15 03:12:46 +0200

    added start/end/wall nodes with their creation with mouse movements

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,14 +1,82 @@
 import Node from './Node.jsx';
 
+import { useState } from 'react';
+
+const isTargetNode = (targetNode, i, j) => {
+    return i == targetNode.row && j == targetNode.col;
+}
+
+const isWall = (i, j, walls) => {
+    return walls.has(JSON.stringify({i, j}));
+}
+
 const Grid = () => {
 
+    const nodeOnMouseDown = (i, j) => {
+        if(isTargetNode(startNode, i, j))
+            setDragStart(true);
+        else if(isTargetNode(endNode, i, j))
+            setDragEnd(true);
+        else
+            setDragWall(true);
+    }
+
+    const nodeOnMouseEnter = (i, j) => {
+        if(dragStart){
+            setStartNode({'row': i, 'col': j});
+        }
+        else if(dragEnd)
+        {
+            setEndNode({'row': i, 'col': j});
+        }
+        else if(dragWall)
+        {
+            walls.add(JSON.stringify({i, j}));
+            setWalls(new Set(walls));
+        }
+    }
+
+    const nodeOnMouseUp = () => {
+        if(dragEnd)
+            setDragEnd(false);
+        else if(dragStart)
+            setDragStart(false);
+        else if(dragWall)
+            setDragWall(false);
+    }
+
     let grid = [];
+
+    const [startNode, setStartNode] = useState({
+        'row': 10, 
+        'col': 5
+    });
+
+    const [endNode, setEndNode] = useState({
+        'row': 10, 
+        'col': 40
+    });
+    
+    const [dragStart, setDragStart] = useState(false);
+    const [dragEnd, setDragEnd] = useState(false);
+    const [dragWall, setDragWall] = useState(false);
+    const [walls, setWalls] = useState(new Set());
+
+    /* Creating the Grid */
     for(let i = 0; i < 30; ++i)
     {
         let row = [];
-        for(let j = 9; j < 50; ++j)
+        for(let j = 0; j < 45; ++j)
         {
-            row.push(<Node row={i} col = {j}></Node>)
+            console.log(isWall(i, j, walls));
+            row.push(<Node row={i} col = {j} 
+                start = {isTargetNode(startNode, i, j)}
+                end = {isTargetNode(endNode, i, j)}
+                wall = {isWall(i, j, walls)}
+                onMouseDown={nodeOnMouseDown}
+                onMouseEnter= {nodeOnMouseEnter}
+                onMouseUp = {nodeOnMouseUp}
+                />)
         }
         grid.push(<div className='flex'>
             {row}
--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -1,9 +1,17 @@
 
 
-const Node = ({ row, col }) => {
+const Node = ({ row, col, start, end, wall, onMouseDown, onMouseEnter, onMouseUp }) => {
+
+    let nodeColor = start ? 'bg-red-500' : 'bg-white';
+    nodeColor = end ? 'bg-green-500' : nodeColor;
+    nodeColor = wall ? 'bg-blue-700' : nodeColor;
 
   return (
-    <div className="w-7 h-6 border border-solid border-blue-200" />
+    <div className = {`w-7 h-6 border border-solid border-blue-200 ${nodeColor}`} 
+        onMouseDown={(ev) => { ev.preventDefault(); onMouseDown(row, col); }}
+        onMouseEnter={(ev) => {ev.preventDefault(); onMouseEnter(row, col);}}
+        onMouseUp={(ev) => {ev.preventDefault(); onMouseUp();}}
+    />
   )
 }
 
